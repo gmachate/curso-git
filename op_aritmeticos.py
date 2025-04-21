dd3d877b53118b7b530fe58f402e8e19f5818a8f
Gary M <119017147+gmachate@users.noreply.github.com>
2025-04-21 10:46:43 -0500

    subiendo archivo de python

---
+++ op_aritmeticos.py
@@ -0,0 +1,21 @@
+#suma y retta(+ y -)
+suma = 12 + 3
+resta = 78 - 4
+
+#multiplicación y división (* y /)
+multi = 5*7
+division = 110/3
+
+#potencia (exponente) (**)
+exponente = 12**2
+
+#división baja (//)
+division_baja = 12//5 #devuelve entero redondeado hacia abajo
+
+#resto o modulo
+resto = 12 % 5
+
+#Type devuelve el tipo de dato
+tipo_de_dato = type(division)
+
+print(division_baja)
