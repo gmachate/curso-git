f2e606ce0ab6fa2dbd5e886121995fdd3abc3149
Gary <garymt05@gmail.com>
2025-04-21 11:11:55 -0500

    cambiando archivo py

--- op_aritmeticos.py
+++ op_aritmeticos.py
@@ -1,21 +1,6 @@
-#suma y retta(+ y -)
-suma = 12 + 3
-resta = 78 - 4
+nombre = "Soy"
+apellido = "Gary"
 
-#multiplicación y división (* y /)
-multi = 5*7
-division = 110/3
+saludo = "hola " +nombre+ " " +apellido+ " ¿Cómo estás?"
 
-#potencia (exponente) (**)
-exponente = 12**2
-
-#división baja (//)
-division_baja = 12//5 #devuelve entero redondeado hacia abajo
-
-#resto o modulo
-resto = 12 % 5
-
-#Type devuelve el tipo de dato
-tipo_de_dato = type(division)
-
-print(division_baja)
+print(saludo)
